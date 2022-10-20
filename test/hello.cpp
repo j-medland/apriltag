15d010b78a042d98f593c83928de30ef6b87428d
John Medland <j-medland@users.noreply.github.com>
2022-10-20 21:19:47 +0000

    Interfaces and builds

--- test/hello.cpp
+++ test/hello.cpp
@@ -1,11 +1,33 @@
 #include <string>
 #include <iostream>
 #include "apriltag/apriltag.h"
+#include "apriltag/tagStandard41h12.h"
+#include "apriltag/common/getopt.h"
 
 int main(int argc, char *argv[])
 {
     std::cout << "hello" << std::endl;
-    auto d = apriltag_detector_create();
-    apriltag_detector_destroy(d);
+    // apriltag_family_t *tf = tagStandard41h12_create();
+    // apriltag_detector_t *td = apriltag_detector_create();
+    // apriltag_detector_add_family_bits(td, tf, 1);
+    // apriltag_detector_destroy(td);
+    // tagStandard41h12_destroy(tf);
+        getopt_t *getopt = getopt_create();
 
+    getopt_add_bool(getopt, 'h', "help", 0, "Show this help");
+    getopt_add_bool(getopt, 'd', "debug", 0, "Enable debugging output (slow)");
+    getopt_add_bool(getopt, 'q', "quiet", 0, "Reduce output");
+    getopt_add_string(getopt, 'f', "family", "tag36h11", "Tag family to use");
+    getopt_add_int(getopt, 'i', "iters", "1", "Repeat processing on input set this many times");
+    getopt_add_int(getopt, 't', "threads", "1", "Use this many CPU threads");
+    getopt_add_int(getopt, 'a', "hamming", "1", "Detect tags with up to this many bit errors.");
+    getopt_add_double(getopt, 'x', "decimate", "2.0", "Decimate input image by this factor");
+    getopt_add_double(getopt, 'b', "blur", "0.0", "Apply low-pass blur to input; negative sharpens");
+    getopt_add_bool(getopt, '0', "refine-edges", 1, "Spend more time trying to align edges of tags");
+
+    if (!getopt_parse(getopt, argc, argv, 1) || getopt_get_bool(getopt, "help")) {
+        printf("Usage: %s [options] <input files>\n", argv[0]);
+        getopt_do_usage(getopt);
+        exit(0);
+    }
 }
